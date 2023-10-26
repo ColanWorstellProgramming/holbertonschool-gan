3dcd11d975c9c036dd4a9dcd5c69b3fa3672f3a4
Colan <cworstellprogramming@gmail.com>
2023-10-26 15:04:54 -0500

    First

---
+++ dcgan/models/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Imports
+"""
+import tensorflow as tf
+from tensorflow import keras
+
+
+# Define a main function
+def main():
+    # Load MNIST dataset from the specified location
+    mnist_data = tf.keras.utils.get_file('MNIST.npz', '../data/MNIST.npz')
+
+    # Load and preprocess the dataset
+    (train_images, _), (_, _) = keras.datasets.mnist.load_data(path=mnist_data)
+    train_images = train_images.astype("float32") / 255.0
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+    BUFFER_SIZE = 60000
+    BATCH_SIZE = 64
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+    # Define your generator and discriminator models here
+    def make_generator_model():
+        # ... (your generator model code)
+        pass
+
+    def make_discriminator_model():
+        # ... (your discriminator model code)
+        pass
+
+    generator = make_generator_model()
+    discriminator = make_discriminator_model()
+
+    # Define loss functions and optimizers for generator and discriminator
+
+    # Training loop and steps go here
+
+if __name__ == "__main__":
+    main()
